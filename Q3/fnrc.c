7eaeef7c6a0db81b1aa767bf0bba66ec3e2cc2ac
Gabriel Felipe Barbosa <40369254+Graail@users.noreply.github.com>
2018-06-18 21:08:22 -0300

    Resposta Questao 3
    Resposta Questao 3

--- Q3/fnrc.c
+++ Q3/fnrc.c
@@ -10,21 +10,44 @@
  */
 char firstNonRepeatingChar (char sentence[], int length)
 {
-    printf("\nfirstNonRepeatingChar::START: %s, %d\n", s, length);
+    printf("\nfirstNonRepeatingChar::START: %s, %d\n", sentence, length);
     
-    // TODO: YOUR CODE HERE!
-    
-    printf("\nfirstNonRepeatingChar::END\n");
+    int i1 = 0; 
+    int i2 = 0;
+    int repete = 1; 
+    char naorepete;
+
+    while(i1 < length){
+    repete = 0; 
+    i2 = 0; 
+
+       
+    while(i2 < length){ 
+    if(i1 != i2){
+    if (sentence[i1]==sentence[i2]){
+    repete = 1;
+    }
+    }
+    i2++;
+    } 
+
+    if (repete == 0){ 
+    naorepete = sentence[i1];
+    printf("%c", naorepete); 
+    return(naorepete);
+    } 
+    i1++;
+    }
+    printf("-1");
     return -1;
 }
 
 int main()
 {
     printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: firstNonRepeatingChar ###\n");
-    
+
     firstNonRepeatingChar("ovo", 3);
     firstNonRepeatingChar("cesar school", 12);
     firstNonRepeatingChar("sistemas digitais", 17);
-    
     return 0;
 }
